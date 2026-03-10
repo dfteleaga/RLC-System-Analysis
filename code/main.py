5da630b8a65cd8e6c79bb9b9a3f0bd55a8fccc22
Daniel <dfteleaga@gmail.com>
2026-03-10 22:07:41 +0000

    Simulation code and case results

--- code/main.py
+++ code/main.py
@@ -3,22 +3,36 @@ from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
 global R, L, C
-R = 0.001
+R_values = [0.5, 2, 5]
 L = 1
 C = 1
 
-def rlc(t, x):
+def rlc(t, x, R, L, C):
     x1 = x[0]
     x2 = x[1]
-    
+        
     dx1 = x2
-    dx2 = -(R/L)*x2 - (1/(L*C))*x1
-    
+    dx2 = -(R/L)*x2 - (1/(L*C))*x1 ## gets state space equations for RLC circuit
+        
     return [dx1, dx2]
 t_span = (0, 10)
 
-sol = solve_ivp(rlc, t_span, [1, 0])
+for R in R_values:
+    sol = solve_ivp(rlc, t_span, [1, 0],args = (R, L, C), t_eval=np.linspace(0, 10, 1000))
 
-plt.plot(sol.t, sol.y[0])
-plt.plot(sol.t, sol.y[1])
-plt.show()
+    plt.plot(sol.t, sol.y[0], label=f"R = {R}") ## plots the current over time for each value of R
+
+    ax = plt.gca()
+
+    plt.xlabel('Time (s)')
+    plt.ylabel('Current (A)')
+
+    ## shift the x and y axes to the zero point so it lines up with the graph
+    ax.spines['left'].set_position('zero') 
+    ax.spines['bottom'].set_position('zero')
+
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+
+    plt.legend()
+    plt.show()
