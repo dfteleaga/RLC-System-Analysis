abf0b58e0dbdd5a47cc352e3705de350b5c40134
Daniel <dfteleaga@gmail.com>
2026-03-23 23:42:42 +0000

    improved simulation code

--- code/Forced_Response.py
+++ code/Forced_Response.py
@@ -17,24 +17,44 @@ def rlc(t, x, R, L, C, V_in):
         
     return [dx1, dx2]
 
-t_span = (0, 10)
+def simulate_rlc(R_values, L, C, V_in, t_span, initial_values):
+    results = {}
 
-for R in R_values:
-    sol = solve_ivp(rlc, t_span, [0, 0] ,args = (R, L, C, V_in), t_eval=np.linspace(0, 10, 1000))
+    for R in R_values:
+        sol = solve_ivp(rlc, t_span, [0, 0] ,args = (R, L, C, V_in), t_eval=np.linspace(0, 10, 1000))
+        results[R] = sol
 
-    plt.plot(sol.t, sol.y[0], label=f"R = {R}") ## plots the current over time for each value of R
+    return results
 
-ax = plt.gca()
+def plot_results(results):
+    for R in R_values:
+        sol = results[R]
+        plt.plot(sol.t, sol.y[0], label=f"R = {R}") ## plots the current over time for each value of R
+    ax = plt.gca()
 
-plt.xlabel('Time (s)')
-plt.ylabel('Current (A)')
 
-## shift the x and y axes to the zero point so it lines up with the graph
-ax.spines['left'].set_position('zero')
-ax.spines['bottom'].set_position('zero')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Current (A)')
 
-ax.spines['right'].set_color('none')
-ax.spines['top'].set_color('none')
+    ## shift the x and y axes to the zero point so it lines up with the graph
+    ax.spines['left'].set_position('zero')
+    ax.spines['bottom'].set_position('zero')
 
-plt.legend()
-plt.show()
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+
+    plt.legend()
+    plt.savefig('forced_response.png')
+    plt.show()
+
+if __name__ == "__main__":
+    R_values = [0.5, 2, 5]
+    L = 1
+    C = 1
+    V_in = 1
+
+    t_span = (0, 10)
+    initial_values = [0, 0]
+
+    results = simulate_rlc(R_values, L, C, V_in, t_span, initial_values)
+    plot_results(results)
--- code/Natural_Response.py
+++ code/Natural_Response.py
@@ -2,12 +2,6 @@ import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
-global R_values, L, C
-R_values = [0.5, 2, 5]
-L = 1
-C = 1
-V_in = 1
-
 def rlc(t, x, R, L, C):
     x1 = x[0]
     x2 = x[1]
@@ -16,24 +10,46 @@ def rlc(t, x, R, L, C):
     dx2 = -(R/L)*x2 - (1/(L*C))*x1  ## gets state space equations for RLC circuit
         
     return [dx1, dx2]
-t_span = (0, 10)
 
-for R in R_values:
-    sol = solve_ivp(rlc, t_span, [1, 0] ,args = (R, L, C), t_eval=np.linspace(0, 10, 1000))
+def simulate_rlc(R_values, L, C, t_span, initial_values):
 
-    plt.plot(sol.t, sol.y[0], label=f"R = {R}") ## plots the current over time for each value of R
+    results = {}
 
-ax = plt.gca()
+    for R in R_values:
+        sol = solve_ivp(rlc, (0,10), [1, 0] ,args = (R, L, C), t_eval=np.linspace(0, 10, 1000))
+        results[R] = sol
 
-plt.xlabel('Time (s)')
-plt.ylabel('Current (A)')
+    return results
 
-## shift the x and y axes to the zero point so it lines up with the graph
-ax.spines['left'].set_position('zero') 
-ax.spines['bottom'].set_position('zero')
+def plot_results(results):
+    plt.figure()
 
-ax.spines['right'].set_color('none')
-ax.spines['top'].set_color('none')
+    for R in R_values:
+        sol = results[R]
+        plt.plot(sol.t, sol.y[0], label=f"R = {R}") ## plots the current over time for each value of R
+    ax = plt.gca()
 
-plt.legend()
-plt.show()
+    plt.xlabel('Time (s)')
+    plt.ylabel('Current (A)')
+
+    ## shift the x and y axes to the zero point so it lines up with the graph
+    ax.spines['left'].set_position('zero') 
+    ax.spines['bottom'].set_position('zero')
+
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+
+    plt.legend()
+    plt.savefig('natural_response.png')
+    plt.show()
+
+if __name__ == "__main__":
+    R_values = [0.5, 2, 5]
+    L = 1
+    C = 1
+
+    t_span = (0, 10)
+    initial_values = [1, 0]
+
+    results = simulate_rlc(R_values, L, C, t_span, [1,0])
+    plot_results(results)
